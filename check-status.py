2283a8fe6c0e5eb0a9502da17fb1d0a67322dc11
Barry Morrison <bdmorrison@gmail.com>
2013-03-16 15:34:41 -0700

    Create check-status.py

---
+++ check-status.py
@@ -0,0 +1,30 @@
+import time
+from subprocess import call
+import socket
+import requests
+
+start_time = time.time()
+HOSTNAME = socket.gethostname()
+
+
+def getStatus():
+    while 1 == 1:
+
+        elapsed_time = time.time() - start_time
+        r = requests.get('http://localhost:8000')
+        if int(elapsed_time) > 20 and r.status_code == 503:
+            call(['salt-call', 'file.comment', '/tmp/haproxy.conf',
+                 'server\ {0}\ '.format(HOSTNAME)])
+            call(['salt-call', 'service.reload', 'haproxy'])
+            print "Now you fix it!"
+            break
+        # elif int(elapsed_time) < 30 and r.status_code == 503:
+        #     print 'fail'
+        #     print elapsed_time
+        else:
+            print r.status_code
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    getStatus()

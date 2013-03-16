688bd53e7e5a087159ce966277728d215bf8eaec
Barry Morrison <bdmorrison@gmail.com>
2013-03-16 15:34:00 -0700

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,25 @@
+from time import time
+import SimpleHTTPServer
+import SocketServer
+
+
+def elapsed_time(_since=time()):
+    return time() - _since
+
+
+class SillyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if elapsed_time() < 30:
+            self.send_error(503)
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'<!DOCTYPE html><html><body><h2>Foo bar</h2></body></html>')
+
+httpd = SocketServer.TCPServer(('localhost', 8000), SillyHandler)
+print('Serving http://{addr[0]}:{addr[1]}/'.format(addr=httpd.socket.getsockname()))
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print('\nKeyboard interrupt received, exiting.')
+    httpd.socket.close()
